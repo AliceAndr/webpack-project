2af27b43844c7e62f94ef73098ccd752360dc0f6
Alice <andreevaalisa42@gmail.com>
2022-07-14 17:09:29 +0300

    setting up the loaders

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import Post from "./Post";
+import json from './assets/json.json';
+import RandomPicture from './assets/photo-1493612276216-ee3925520721.jpeg';
+import './styles/styles.css';
 
-const post = new Post('Webpack title');
+const post = new Post('Webpack title', RandomPicture);
 
 console.log('Post to String', post.toString());
+
+console.log('JSON', json);
